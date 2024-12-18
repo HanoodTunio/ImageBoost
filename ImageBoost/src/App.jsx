8976fecf56cfc4d7f5f5234bf0364ff443e74975
hanood_tunio <hanood.tunio@gmail.com>
2024-12-18 14:59:51 +0500

    ImageBoost project setup with Material-UI

---
+++ ImageBoost/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Vite + React + Material-UI</h1>
+      <Button variant="contained" color="primary">
+        Hello Material-UI
+      </Button>
+    </div>
+  );
+}
+
+export default App;
